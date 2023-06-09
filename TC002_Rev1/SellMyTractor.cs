f8889324dc0d993e1575197c512fdae1862bdd7c
Benjamin Schatzmann <benjamin.schatzmann@progile.ch>
2023-06-09 16:34:06 +0200

    TC002 added

---
+++ TC002_Rev1/SellMyTractor.cs
@@ -0,0 +1,15 @@
+﻿[TestCase(1)]
+public class SellMyTractor : TestCase
+{
+    [TestStep(1)]
+    public void Step1(ITester t)
+    {
+        //same precondition as tc001
+        //Open Menu -> click Trucks
+        //validate browser url switched to trucks
+        //click sell now (the one below because the one on top requires a login?)
+        //enter some info
+        //click next -> check validation messages (with test case images?)
+        t.Report.PassStep("Hello World!");
+    }
+}
