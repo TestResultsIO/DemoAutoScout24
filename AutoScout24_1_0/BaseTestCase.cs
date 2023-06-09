f8b25ae73bdbb72a4349fa5a4be862214a148614
Benjamin Schatzmann <benjamin.schatzmann@progile.ch>
2023-06-09 12:05:04 +0200

    Solution Template

---
+++ AutoScout24_1_0/BaseTestCase.cs
@@ -0,0 +1,34 @@
+﻿using Progile.ATE.TestFramework;
+
+namespace AutoScout24_Model
+{
+    public class TestCase
+    {
+        protected AutoScout24App App { get; set; }
+
+        [SetupTest]
+        public virtual bool Setup(ITester t)
+        {
+            App = new AutoScout24App(t);
+            return true;
+        }
+
+        //[PreconditionStep(
+        //    TestInput = "",
+        //    ExpectedResults = "")]
+        //public virtual void PreconditionStep(ITester t)
+        //{ }
+
+        //[CleanupStep(
+        //    TestInput = "",
+        //    ExpectedResults = "")]
+        //public virtual void CleanupStep(ITester t)
+        //{ }
+
+        //[TearDownTest]
+        //public virtual bool TearDown(ITester t)
+        //{
+        //    return true;
+        //}
+    }
+}
---
+++ TC001_Rev1/RefineSearch.cs
@@ -0,0 +1,9 @@
+﻿[TestCase(1)]
+public class RefineSearch : TestCase
+{
+    [TestStep(1)]
+    public void Step1(ITester t)
+    {
+        t.Report.PassStep("Hello World!");
+    }
+}
