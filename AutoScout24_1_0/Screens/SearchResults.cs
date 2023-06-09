cbb4a5c0fcaec00dec81866fe20d30078b95ca2f
Benjamin Schatzmann <benjamin.schatzmann@progile.ch>
2023-06-09 15:54:16 +0200

    Testcase RefineSearch

---
+++ AutoScout24_1_0/Screens/SearchResults.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using AutoScout24_Model.Helpers;
+using Progile.ATE.Common;
+using Progile.ATE.TestFramework;
+using Progile.TRIO.BaseModel;
+using Progile.TRIO.EnvironmentModel;
+
+using static TestImages.AutoScout24;
+
+
+namespace AutoScout24_Model.Screens
+{
+	public partial class SearchResults
+	{
+        partial void ConfigureElementProperties()
+        {
+            OffersFound.ValueOcrParas = OcrParams.TextAlternative;
+        }
+    }
+}
--- TC001_Rev1/RefineSearch.cs
+++ TC001_Rev1/RefineSearch.cs
@@ -1,9 +1,42 @@
 ﻿[TestCase(1)]
 public class RefineSearch : TestCase
 {
-    [TestStep(1)]
+    [PreconditionStep(TestInput = @"Browser is open with https://www.autoscout24.com/",
+        ExpectedResults = @"AutoScout24 Main Page was found")]
+    public void PreconditionStep(ITester t)
+    {
+        /*
+         * if the test is executed on the portal the browser will be opened
+         * automatically with the https://www.autoscout24.com/ as it is defined in the Webpage SoftwareModel
+         * 
+         * if you run this testcase in Visual Studio make sure to connect 
+         * to your test environmentand open https://www.autoscout24.com/ there in Microsoft Edge
+        */
+        t.Report.PassFailStep(App.MainPage.WaitForAppear(), $@"The element {/*element*/ App.MainPage} was displayed on the screen.", $@"The element {/*element*/ App.MainPage} was not found on the screen.");
+    }
+
+    [TestStep(1,
+        TestInput = @"Click the Link 'Refine Search'",
+        ExpectedResults = @"The Detail Search Screen opens")]
     public void Step1(ITester t)
     {
-        t.Report.PassStep("Hello World!");
+        App.MainPage.RefineSearchLink.Click(App.DetailSearch.WaitForAppear);
+        t.Report.PassStep($@"The Detail Search Screen is open");
+    }
+
+
+    [TestStep(2,
+        TestInput = @"Search for Rolls-Royce Ghost (used, V12, with Seat heating)",
+        ExpectedResults = @"Report how many Offers there are")]
+    public void Step2(ITester t)
+    {
+        App.DetailSearch.MakeDropdownWithSearchbox.SelectValueWithSearch($@"Ro", $@"Rolls-Royce");
+        App.DetailSearch.ModelDropdownWithSearchbox.SelectValue($@"Ghost");
+        App.DetailSearch.VariantTextbox.Enter($@"V12");
+        App.DetailSearch.UsedCheckbox.Check();
+        App.DetailSearch.SeatHeatingCheckbox.Check();
+        App.DetailSearch.ResultsButton.Click(App.SearchResults.WaitForAppear);
+        var numberOfOffers = App.SearchResults.OffersFound.ReadValue();
+        t.Report.PassStep($@"Found {numberOfOffers} Offers");
     }
 }
