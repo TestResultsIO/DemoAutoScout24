00870c557b4ba40dd9e2a2bb860f3d5ef969b588
Benjamin Schatzmann <benjamin.schatzmann@progile.ch>
2023-06-16 14:05:24 +0200

    TC003 with Button Example Interactions

--- AutoScout24_1_0/Screens/AutoScout24 Screens/MainPage.g.cs
+++ AutoScout24_1_0/Screens/AutoScout24 Screens/MainPage.g.cs
@@ -19,12 +19,18 @@ namespace AutoScout24_Model.Screens.AutoScout24Screens
         public MainPage(IAppBasics appBasics) : base(appBasics, @"Main Page", Images.AutoScout24Screens.MainPage.Screen_Loaded)
         {
             RefineSearchLink = new Progile.TRIO.BaseModel.Button(tester: t, displayName: "Refine search Link", activeImageReference: Images.AutoScout24Screens.MainPage.RefineSearchLink.active, useVisualSense: false, filters: ScreenSelect) { ParentElement = this };
+            ResultsButton = new Progile.TRIO.BaseModel.Button(tester: t, displayName: "ResultsButton", activeImageReference: Images.AutoScout24Screens.MainPage.ResultsButton.active, useVisualSense: false, filters: ScreenSelect) { ParentElement = this };
+            PriceUpToDropdown = new Progile.TRIO.BaseModel.Dropdown(appBasics: AppBasics, displayName: "PriceUpToDropdown", imageReference: Images.AutoScout24Screens.MainPage.PriceUpToDropdown.DropdownTextBox, useVisualSense: true, filters: ScreenSelect) { ParentElement = this };
 
             ConfigureElementProperties();
         }
 
 
         public Progile.TRIO.BaseModel.Button RefineSearchLink { get; private set; }
+
+        public Progile.TRIO.BaseModel.Button ResultsButton { get; private set; }
+
+        public Progile.TRIO.BaseModel.Dropdown PriceUpToDropdown { get; private set; }
 
         partial void ConfigureElementProperties();
     }
