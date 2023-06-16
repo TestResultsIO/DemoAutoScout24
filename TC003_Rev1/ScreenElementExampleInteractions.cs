3d122f1fee5ce2c7162724f853c891e42ef75bc1
Benjamin Schatzmann <benjamin.schatzmann@progile.ch>
2023-06-16 14:05:24 +0200

    TC003 with Button Example Interactions

---
+++ TC003_Rev1/ScreenElementExampleInteractions.cs
@@ -0,0 +1,46 @@
+﻿[TestCase(1)]
+public class ScreenElementExampleInteractions : TestCase
+{
+    /// <summary>
+    /// this testcase is not supposed to run
+    /// just to show some possible interactions with each Screen Element
+    /// </summary>
+
+    // Button Interactions Examples
+    [TestStep(1, TestInput ="Button Interactions")]
+    public void Step1(ITester t)
+    {
+        //Each click on a Button requires a Verification
+        //if the verification is not fulfilled after 20sec, a retry of the click is automatically performed
+        //by default there are 3 tries, but you can configure it with the property "RetryCount" on the Button
+
+        //in most cases this verification is that a new Screen (or element) appears
+        App.MainPage.ResultsButton.Click(App.SearchResults.WaitForAppear);
+
+        //or you can also check if an element disappears
+        App.MainPage.ResultsButton.Click(App.MainPage.WaitForDisappear);
+
+        //if there is really nothing that appears or disappears you can check that at least the screen updates
+        App.MainPage.ResultsButton.ClickWithUpdateCheck(ImgDiffTolerance.Medium);
+
+        //if you have a Button with different Active and Inactive states and recorded their Images
+        //you can check the current state and depending on it continue on a different path
+        if (App.MainPage.ResultsButton.IsActive())
+        {
+            App.MainPage.ResultsButton.Click(App.SearchResults.WaitForAppear);
+        }
+        else
+        {
+            App.MainPage.RefineSearchLink.Click(App.DetailSearch.WaitForAppear);
+        }
+
+        //IsActive is the current state, in case you want to verify that it changes the state use WaitForActive / WaitForInactive
+        //for example after we selected something in a dropdown, we expect the Button to become active
+        App.MainPage.PriceUpToDropdown.SelectValue("5,000");
+        t.Report.PassFailStep(
+            App.MainPage.ResultsButton.WaitForActive(),
+            "The Results Button became active after selecting a price range.",
+            "The Results Button was not active after selecting a price range."
+        );
+    }
+}
