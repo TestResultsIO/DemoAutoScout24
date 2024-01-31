20ca4abc2c10808bcb52333c0d11c5de36b13724
Benjamin Schatzmann <benjamin.schatzmann@progile.ch>
2024-01-31 17:23:17 +0100

    TC007 PDF Validation

---
+++ TC007_Rev1/PDFValidation.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using TC007_Rev1;
+using Windows_Model;
+using static TC007_Rev1.PdfValidationHelper;
+
+[TestCase(1)]
+public class PDFValidation
+{
+    private string _pdfPathOnHost;
+    private WindowsApp App;
+
+    [SetupTest]
+    public virtual bool Setup(ITester t)
+    {
+        App = new WindowsApp(t);
+        return true;
+    }
+
+    [TestStep(1, TestInput = "Make pdf file accessible for Execution Host")]
+    public void Step1(ITester t)
+    {
+        //the goal is to parse the .pdf file with the used iText7 Nuget
+        //for that the execution host (where to code is running) needs access to the pdf file
+        //the following code copies the file from the System Under Test (SUT) to the Execution host using the RemoteDirectory
+
+        //Make sure the connection string to the RemoteDirectory is correctly set on the Test Environment
+        //https://docs.testresults.io/designer/interacting-with-the-sut/tabs-overview/environments#remote-directory-connection-string
+
+        string pdfPath = @"C:\\Temp\\";
+        string pdfFileName = "myPdf.pdf";
+        App.SystemHelpers.SetUpRemoteDirectory(true);
+        App.SystemHelpers.CopyFiles(Path.Combine(pdfPath, pdfFileName), App.SystemHelpers.ArtifactsDirectory);
+        var remoteDirectoryOnHost = BaseModelHelpers.MapRemoteDirectoryOnHost(t);
+        if (!remoteDirectoryOnHost.Exists || remoteDirectoryOnHost.GetFiles(pdfFileName).Length == 0)
+            throw new TestStepAbortedException($"Could not read {pdfFileName} on host from remoteDirectory");
+
+        var _pdfPathOnHost = Path.Combine(remoteDirectoryOnHost.FullName, pdfFileName);
+    }
+
+
+    [TestStep(2,
+        TestInput = "Verify the content of the pdf",
+        ExpectedResults = "The pdf content is as expected")]
+    public void Step2(ITester t)
+    {
+        var helper = new PdfValidationHelper(t);
+        string pdfPath = _pdfPathOnHost ?? "myPdf.pdf"; //the variable set in TestStep 1 should be used, but for demo purposes also the pdf project item works
+        var expectedPdfContent = new List<ExpectedPDFContent>() 
+        { 
+            new ExpectedPDFContent(page: 1, line: 13, "Maecenas mauris lectus, lobortis et purus mattis, blandit dictum tellus."),                     // expected to pass
+            new ExpectedPDFContent(page: 1, line: 13, "Maecenas mauris lectus, lobortis et purus mattis, blandit dictum tellus"),                      // expected to fail, missing the . at the end
+            new ExpectedPDFContent(page: 3, line: 20, "In non mauris justo. Duis vehicula mi vel mi pretium, a viverra erat efficitur. Cras aliquam"), // expected to fail, text is different
+            new ExpectedPDFContent(page: 3, line: 50, "Lorem ipsum dolor sit amet, consectetur adipiscing elit."),   // expected to fail, only 38 lines on page 3
+            new ExpectedPDFContent(page: 5, line: 20, "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")    // expected to fail only 4 pages
+        };
+        helper.ValidatePDFAndReport(pdfPath, expectedPdfContent);
+    }
+
+    [CleanupStep(TestInput = "Unmap the Remote Directory on Host")]
+    public virtual void CleanupStep(ITester t)
+    {
+        BaseModelHelpers.UnmapRemoteDirectoryOnHost(t);
+    }
+}
---
+++ TC007_Rev1/PdfValidationHelper.cs
@@ -0,0 +1,108 @@
+﻿using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TC007_Rev1
+{
+    public class PdfValidationHelper
+    {
+        public struct ExpectedPDFContent
+        {
+            public int Page { get; private set; } //start with 1 not with 0
+            public int Line { get; private set; } //start with 1 not with 0
+            public string ExpectedText { get; private set; }
+
+            public ExpectedPDFContent(int page, int line, string expectedText)
+            {
+                Page = page;
+                Line = line;
+                ExpectedText = expectedText;
+            }
+
+        }
+
+        private struct PDFPage
+        {
+            public readonly string[] Lines { get; }
+
+            public PDFPage(string[] lines)
+            {
+                Lines = lines;
+            }
+        }
+
+        private ITester t;
+        public PdfValidationHelper(ITester tester)
+        {
+            t = tester;
+        }
+
+        private string GetPdfContentAsString(string pdfFilePath)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            var pdfDocument = new PdfDocument(new PdfReader(pdfFilePath));
+            for (int i = 1; i <= pdfDocument.GetNumberOfPages(); i++)
+            {
+                var page = pdfDocument.GetPage(i);
+                string text = PdfTextExtractor.GetTextFromPage(page);
+                string processed = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(text)));
+                stringBuilder.Append(processed);
+            }
+            pdfDocument.Close();
+            return stringBuilder.ToString();
+        }
+
+        private List<PDFPage> GetPDFContent(string pdfFilePath) 
+        { 
+            var res = new List<PDFPage>();
+            var pdfDocument = new PdfDocument(new PdfReader(pdfFilePath));
+            for (int i = 1; i <= pdfDocument.GetNumberOfPages(); i++)
+            {
+                var page = pdfDocument.GetPage(i);
+                string text = PdfTextExtractor.GetTextFromPage(page);
+                string processed = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(text)));
+                string[] lines = processed.Split(new string[] { "\n" }, StringSplitOptions.None);
+                res.Add(new PDFPage(lines));
+            }
+            return res;
+        }
+
+        private bool Validate(List<PDFPage> pdfPages, ExpectedPDFContent expectedPDFContent, out string actualText)
+        {
+            if(pdfPages.Count < expectedPDFContent.Page)
+            {
+                actualText = $"Expected {expectedPDFContent.Page} pages in the pdf, but only got {pdfPages.Count}";
+                return false;
+            }
+            PDFPage pdfPage = pdfPages[expectedPDFContent.Page-1];
+            if(pdfPage.Lines.Length < expectedPDFContent.Line)
+            {
+                actualText = $"Expected {expectedPDFContent.Line} lines on pdf page {expectedPDFContent.Page}, but only got {pdfPage.Lines.Length}";
+                return false;
+            }
+            actualText = pdfPage.Lines[expectedPDFContent.Line - 1];
+            return actualText.Equals(expectedPDFContent.ExpectedText);
+        }
+
+        public void ValidatePDFAndReport(string pdfFilePath, List<ExpectedPDFContent> expectedContent)
+        {
+            List<PDFPage> pdfContent = GetPDFContent(pdfFilePath);
+            foreach (ExpectedPDFContent expected in expectedContent)
+            {
+                t.Report.PassFailStep(
+                    Validate(pdfContent, expected, out string actualText),
+                    $"The pdf content was as expected, Page: {expected.Page} Line: {expected.Line} ExpectedText: '{expected.ExpectedText}'",
+                    $"The pdf content was not as expected, {expected.Page} Line: {expected.Line} ExpectedText: '{expected.ExpectedText}' ActualText: '{actualText}'",
+                    false
+                );
+            }
+        }
+    }
+
+}
