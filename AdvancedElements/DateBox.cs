0fce8320359ca69ee03239c58cff60eef68d3868
Pascal Gyger <pascal.gyger@progile.ch>
2024-11-18 15:55:30 +0100

    Improve nuget package for AdvancedElements

--- AdvancedElements/DateBox.cs
+++ AdvancedElements/DateBox.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace AdvancedElements
+namespace Progile.TRIO.AdvancedElements
 {
     [ScreenElements(
     "appBasics:AppBasics",
---
+++ AdvancedElements/README.md
@@ -0,0 +1,2 @@
+#Introduction
+This package provides advanced elements to extend the element types provided by the Base Model.
